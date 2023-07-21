2b0e17bc9397a4b5f5f24fb2828320b776edfab1
amare-en <amareengidawork@gmail.com>
2023-07-21 14:34:41 +0300

    quizes project

--- Quiz/src/App.vue
+++ Quiz/src/App.vue
@@ -1,85 +1,77 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {ref, watch} from "vue"
+import q from "./data/Quiz.json"
+//import Card from "./components/Card.vue" 
+const Quiz = ref(q)
+const search = ref("") 
+watch(search, ()=>{
+  console.log("hello from watch")
+  Quiz.value = q.filter(quizes=> quizes.name.toLowerCase().includes(search.value.toLowerCase()))
+})
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+  <div class="container">
+  <h1>
+  Quizes:
+  </h1>
+  <input v-model.trim ="search" type="text" placeholder="search..."/>
+  <header/> 
+  <div class="options-container">
+  <div class="cards" v-for="quizes in Quiz" :key="quizes.id">
+  <img :src="quizes.img" alt="">
+  <div class="card-text"> 
+  <h2>
+  {{quizes.name}}
+ </h2>
+ <p>{{quizes.questions.length}} questions</p>
+ </div>
+ </div>
+ </div>
+ </div>
+ </template>
+ <style>
+ .container{
+ max-width: 1000px;
+ margin: 0 auto;
+}
+header{
+  margin-top: 30px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+header h1{
+  font-weight: bold;
+  margin-right: 30px;
+}
+header input{
+  border: none;
+  background-color: antiquewhite;
+  padding: 10px;
+  border-radius: 5px;
+}
+.options-container{
+display: flex;
+margin: 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+/*cards*/
+.cards{
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px black;
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.card img{
+  width:100%;
+  height:190px;
+  margin: 0;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.card .card-text h2{
+  padding: 0 5 px;
+  font-weight: bold;
 }
 </style>
---
+++ Quiz/src/views/Card.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {defineProps} from "vue"
+const {quiz} = defineProps(["quiz"])
+console.log(quiz)
+</script>
+<template>
+   <div class="cards">
+   <img :src="quiz.img">
+   <div class="card-text">
+       <h1>
+     {{quiz.name}}
+       </h1>
+    </div>
+    </div>
+</template>
+<style scoped>
+.cards{
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px black;
+    margin-bottom: 35px;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+</style>
